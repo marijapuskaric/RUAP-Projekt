ea9d85596a5ba0580402e9ea15b7f4b81d771aaa
Nikola <marija.puskaric157@gmail.com>
2022-10-25 18:14:50 +0200

    drugi commit

--- RUAP/Program.cs
+++ RUAP/Program.cs
@@ -6,7 +6,7 @@ namespace RUAP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Marije Puškarić");
+            Console.WriteLine("Pozdrav iz aplikacije od Marije Puškarić 157157157");
         }
     }
 }
