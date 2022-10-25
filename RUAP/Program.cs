661b55d2268a07767fa7e02bad0bafadd6c04409
Nikola <marija.puskaric157@gmail.com>
2022-10-25 18:04:30 +0200

    pozdrav

---
+++ RUAP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Marije Puškarić");
+        }
+    }
+}
